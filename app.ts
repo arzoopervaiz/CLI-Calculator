4441baf5032ef15442e661bf766d9e2060c1dff5
Arzoo Pervaiz <161163183+arzoopervaiz@users.noreply.github.com>
2024-03-21 23:04:46 +0500

    first commit

---
+++ app.ts
@@ -0,0 +1,27 @@
+import inquirer from "inquirer";
+import chalk from "chalk";
+const userInput = await inquirer.prompt([
+  { name: "num1", type: "number", message: "Enter a first number:" },
+  { name: "num2", type: "number", message: "Enter your second number:" },
+  {
+    name: "operators",
+    type: "list",
+    message: "Select your operator:",
+    choices: ["Addition","Subtraction","Multiplication", "Division","Exponentiation","Modulus"]
+  },
+]);
+if (userInput.operators === "Addition") {
+  console.log(userInput.num1 + userInput.num2);
+} else if (userInput.operators === "Subtraction") {
+  console.log(userInput.num1 - userInput.num2);
+} else if (userInput.operators === "Multiplication") {
+  console.log(userInput.num1 * userInput.num2);
+} else if (userInput.operators === "Division") {
+  console.log(userInput.num1 / userInput.num2);
+} else if (userInput.operators === "Exponentiation") {
+  console.log(userInput.num1 ** userInput.num2);
+} else if (userInput.operators === "Modulus") {
+  console.log(userInput.num1 % userInput.num2);
+}else{
+  console.log(chalk.red("Error"));
+}
